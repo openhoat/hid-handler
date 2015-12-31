c0a0608c0983d889a701c03408992f2474a40abd
Olivier Penhoat <openhoat@gmail.com>
2015-12-31 21:06:45 +0100

    tests

--- spec/hidHandlerSpec.js
+++ spec/hidHandlerSpec.js
@@ -9,13 +9,26 @@ var chai = require('chai')
 
 describe('hid-handler', function () {
 
-  it('should provide error classes', function () {
+  it('should be ok', function () {
     expect(hidHandler).to.be.ok;
+  });
+
+  it('should provide error classes', function () {
     expect(hidHandler).to.have.property('HidHandlerError');
     expect(hidHandler).to.have.property('HidAlreadyRegisteredError');
     expect(hidHandler).to.have.property('HidEventHandlerNameError');
     expect(new hidHandler.HidHandlerError()).to.be.an.instanceof(hidHandler.HidHandlerError).and.is.an.instanceof(Error);
     expect(new hidHandler.HidAlreadyRegisteredError()).to.be.an.instanceof(hidHandler.HidAlreadyRegisteredError).and.is.an.instanceof(hidHandler.HidHandlerError).and.is.an.instanceof(Error);
+  });
+
+  it('should provide event classes', function () {
+    expect(hidHandler).to.have.property('GenericEvent');
+    expect(hidHandler).to.have.property('KeyboardEvent');
+    expect(hidHandler).to.have.property('MouseEvent');
+    expect(hidHandler).to.have.property('TouchpadEvent');
+    expect(new hidHandler.KeyboardEvent()).to.be.an.instanceof(hidHandler.KeyboardEvent).and.is.an.instanceof(hidHandler.GenericEvent);
+    expect(new hidHandler.MouseEvent()).to.be.an.instanceof(hidHandler.MouseEvent).and.is.an.instanceof(hidHandler.GenericEvent);
+    expect(new hidHandler.TouchpadEvent()).to.be.an.instanceof(hidHandler.TouchpadEvent).and.is.an.instanceof(hidHandler.GenericEvent);
   });
 
 });
